413a3e29cde63fcf3b593b6c21cac9fffa299b00
Methuz Kaewsai-kao <methuz.k@gmail.com>
2021-01-29 02:35:43 +0700

    Fix author component

--- components/Author.vue
+++ components/Author.vue
@@ -1,20 +1,29 @@
 <template>
-  <div class="w-full px-2 xs:mb-6 md:mb-12 article-card">
+  <div class="w-full px-2 xs:mb-6 xl:mb-12 article-card">
     <NuxtLink
       :to="`/blog/author/${author.name}`"
-      class="flex transition-shadow duration-150 ease-in-out shadow-sm hover:shadow-md xxlmax:flex-col"
+      class="rounded-xl flex transition-shadow duration-150 bg-blue-50 ease-in-out shadow-sm shadow-lg xxlmax:flex-col"
     >
-      <img
-        v-if="author.img"
-        class="h-48 xxlmin:w-1/2 xxlmax:w-full object-cover"
-        :src="author.img"
-      />
-      <div class="flex flex-col m-4">
-        <h4 class="font-semibold">Author</h4>
-
-        <p>{{ author.name }}</p>
-        <p>{{ author.bio }}</p>
-      </div>
+      <figure class="xl:flex p-8 xl:p-0">
+        <img
+          class="w-32 h-32 xl:w-48 xl:h-auto xl:rounded-none rounded-full mx-auto object-cover"
+          :src="author.img"
+          alt=""
+          width="384"
+          height="512"
+        />
+        <div class="pt-6 xl:p-8 text-center xl:text-left space-y-4">
+          <blockquote>
+            <p class="text-lg font-semibold">
+              “Talk is cheap, tell me a joke.”
+            </p>
+          </blockquote>
+          <figcaption class="font-medium">
+            <div class="text-cyan-600">{{ author.name }}</div>
+            <div class="text-gray-500">{{ author.bio }}</div>
+          </figcaption>
+        </div>
+      </figure>
     </NuxtLink>
   </div>
 </template>
